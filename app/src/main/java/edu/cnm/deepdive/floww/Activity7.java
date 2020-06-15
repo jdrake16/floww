7dd1aa17da26d331283ad6b5cef9c39280823507
jdrake16 <stovetops111@gmail.com>
2020-06-14 19:52:02 -0600

    Added media player to activity 6,7.

---
+++ app/src/main/java/edu/cnm/deepdive/floww/Activity7.java
@@ -0,0 +1,56 @@
+package edu.cnm.deepdive.floww;
+
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity7 extends AppCompatActivity {
+  MediaPlayer player;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_7);
+  }
+
+  public void play(View v) {
+    if (player == null) {
+      player = MediaPlayer.create(this, R.raw.chronfour);
+      player.setOnCompletionListener(new OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+          stopPlayer();
+        }
+      });
+    }
+
+    player.start();
+  }
+
+  public void pause(View v) {
+    if (player != null){
+      player.pause();
+    }
+  }
+
+  public void stop(View v) {
+    stopPlayer();
+  }
+
+  private void stopPlayer() {
+    if (player != null ) {
+      player.release();
+      player = null;
+      Toast.makeText(this, "MediaPlayer released", Toast.LENGTH_SHORT).show();
+    }
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    stopPlayer();
+  }
+}
--- app/src/main/java/edu/cnm/deepdive/floww/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/floww/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     Button butt1 = findViewById(R.id.button);
     Button butt2 = findViewById(R.id.button2);
     Button butt3 = findViewById(R.id.button3);
+    Button butt4 = findViewById(R.id.button4);
+    Button butt5 = findViewById(R.id.button5);
 
     butt1.setOnClickListener(new OnClickListener() {
       @Override
@@ -40,6 +42,22 @@ public class MainActivity extends AppCompatActivity {
       public void onClick(View v) {
         Intent int3 = new Intent(MainActivity.this,Activity5.class);
         startActivity(int3);
+      }
+    });
+
+    butt4.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent int4 = new Intent(MainActivity.this,Activity6.class);
+        startActivity(int4);
+      }
+    });
+
+    butt5.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent int5 = new Intent(MainActivity.this,Activity7.class);
+        startActivity(int5);
       }
     });
 
